92a304f22feca895ca9be9a6591dda35972e8ae4
Jaeem Ekram <jekram98@gmail.com>
2023-12-02 13:41:50 -0500

    pushes code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id attribute of the <h1> element to 'victory'
+newHeader.setAttribute('id', 'victory');
+
+// Set the text content of the <h1> element to "YOUR-NAME is the champion"
+const yourName = "John Doe"; // Replace 'John Doe' with your desired name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Append the <h1> element to the document body (or any other desired location)
+document.body.appendChild(newHeader);
